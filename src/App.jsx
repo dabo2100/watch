bac61284b37596e2ca747d9c55d883abfd5a235b
Dabo-2100 <a_fattah_m@icloud.com>
2025-10-08 20:44:47 +0300

    Add Routes to Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainLayout from './MainLayout';
+
 export default function App() {
-  return <div>Welcome To The App</div>;
+  return (
+    <div className="w-full h-dvh overflow-auto bg-white text-black">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<h1>Home Page</h1>} />
+            <Route path="products" element={<h1>Home Page</h1>} />
+            <Route path="products/:category_id" element={<h1>Products By Category</h1>} />
+            <Route path="products/:product_id" element={<h1>Prodcut Details</h1>} />
+            <Route path="about" element={<h1>About Us Page Details</h1>} />
+          </Route>
+          <Route path="*" element={<h1>404 Page | Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="w-full flex flex-col items-center">
+      <div className="container bg-red-500">Upper Bar</div>
+      <div className="container bg-blue-500">Lower Bar</div>
+    </div>
+  );
+}
