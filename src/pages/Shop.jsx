e7b61709cf37e3e00c29ba06284f03a62642f29f
Mohamedkamel7413 <mr.mohamedkamel7413@gmail.com>
2025-10-09 13:08:43 +0300

    M.Kamel2

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -77,7 +77,7 @@ export default function Shop() {
               </div>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-              {[1, 2, 3, 4, 5, 6].map((id) => (
+              {[1, 2, 3, 4, 5, 6 , 7, 8, 9].map((id) => (
                 <div
                   key={id}
                   className="bg-white rounded-lg shadow hover:shadow-lg transition overflow-hidden group"
