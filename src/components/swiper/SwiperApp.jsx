6f193bd3611ade65c83d659efb57800c17eb4968
safooahmed1 <safoo468@gmail.com>
2025-10-10 06:36:21 +0300

    DoneHero

--- src/components/swiper/SwiperApp.jsx
+++ src/components/swiper/SwiperApp.jsx
@@ -45,23 +45,67 @@ export default function SwiperApp() {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 3000,
           disableOnInteraction: false,
         }}
         pagination={{
           clickable: true,
         }}
-        navigation={true}
+        navigation={false} //theArorr
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
         {slide.map((el, index) => (
-          <SwiperSlide className="bg-yellow-400">
-            <div className="w-[1279px] h-[660px] max-h-[660px] flex flex-row justify-around items-center bg-blue-300 ">
-              <div className="">{el.title}</div>
+          <SwiperSlide className="bg-[#F6F7FB]">
+            <div className="con w-[1279px] h-[660px] max-h-[660px] flex flex-row justify-around items-center bg-[#F6F7FB] ">
+              <div className="tag-wrapper fixed bottom-25 flex w-full text-[190px] font-extrabold leading-[250px] text-[#EBECF0]">
+                {"TAGHEUER".split("").map((ch, i) => (
+                  <span key={i} className="tag-letter" style={{ "--order": i }}>
+                    {ch}
+                  </span>
+                ))}
+              </div>
 
-              <div>
-                <img src={el.item} alt="" />
+              <div className="flex flex-row justify-around items-center z-10 w-full">
+                <div className="flex flex-col items-start text-start w-1/2 gap-4">
+                  <h2
+                    className="text-[60px] font-bold leading-[70px] stagger-item"
+                    style={{ "--order": 1 }}
+                  >
+                    {el.title}
+                  </h2>
+
+                  <p
+                    className="text-[#666666] text-[16px] leading-[28px] font-medium stagger-item"
+                    style={{ "--order": 2 }}
+                  >
+                    {el.disc}
+                  </p>
+
+                  <div className="stagger-item" style={{ "--order": 3 }}>
+                    <span className="text-[#EB6A2A] font-bold text-[36px] leading-[36px]">
+                      {el.pric}
+                    </span>
+                    <span className="text-[#A6A6A6] text-[24px] leading-[25px] line-through">
+                      {el.oldPric}
+                    </span>
+                  </div>
+
+                  <button
+                    className="py-4 px-10  bg-[#EB6A2A] hover:bg-[#7d3410] w-fit text-[16px] text-white font-medium leading-[17px] text-center stagger-item"
+                    style={{ "--order": 4 }}
+                  >
+                    view collection
+                  </button>
+                </div>
+
+                <div className="stagger-item img" style={{ "--order": 5 }}>
+                  <img
+                    src={el.item}
+                    alt=""
+                    className="w-[500px] h-[500px] object-contain hover:scale-105 transition-transform duration-300 ease-in-out"
+                  />
+                </div>
               </div>
             </div>
           </SwiperSlide>
